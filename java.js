fda5e8349116f5439449deb7ceee38f846196e85
maninhomarcos <111518436+maninhomarcos@users.noreply.github.com>
2023-06-14 11:14:13 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,11 @@
+function renata(){
+document.getElementById('demo').innerHTML = Date();
+}
+
+function thaisplodindo(){
+document.getElementById('mol').src='botao-verde.png';
+}
+
+function sugareletodo(){
+document.getElementById('mol').src='botao-vermelho.png';
+}
